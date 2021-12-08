b52cb4a74c84a826f39688294e7a95a4c5962661
Sebastian Sukiennik <sebastiansukiennik01@gmail.com>
2021-12-09 00:33:11 +0100

    Downloading historical data works with multiple tokens, adressess

--- PlikTestowy.py
+++ PlikTestowy.py
@@ -7,6 +7,7 @@ import json
 import glob
 import pandas as pd
 import collections
+import time
 import matplotlib.pyplot as plt
 
 pd.set_option("display.max_rows", None)
@@ -16,7 +17,7 @@ pd.set_option("display.width", None)
 
 class BitQuery:
 
-    def __init__(self):
+    def __init__(self, baseAddress, quoteAddress, from_date, minute_interval):
         """
         Initializes BitQuery object with headers: API key and query: contains query expression, which states which parameters (symbol, address, open, close, etc.)
         should be returned
@@ -27,29 +28,18 @@ class BitQuery:
   ethereum(network: bsc) {
     dexTrades(
       options: { asc: "timeInterval.minute"}
-      date: {since: "2021-11-01"}
+      date: {since: """ + f'"{from_date}"' + """}
       exchangeName: {is: "Pancake v2"}
-      baseCurrency: {is: "0xD302c09BC32aEF53146B6bA7BC420F5CACa897f6"}
-      quoteCurrency: {is: "0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c"}
+      baseCurrency: {is: """ + f'"{baseAddress}"' + """}
+      quoteCurrency: {is: """ + f'"{quoteAddress}"' + """}
     ) {
       timeInterval {
-        minute(count: 5)
-      }
-      baseCurrency {
-        symbol
-        address
+        minute(count: """ + f"{minute_interval}" + """)
       }
       baseAmount
-      quoteCurrency {
-        symbol
-        address
-      }
-      quoteAmount
       trades: count
-      quotePrice
       maximum_price: quotePrice(calculate: maximum)
       minimum_price: quotePrice(calculate: minimum)
-      median_price: quotePrice(calculate: median)
       open_price: minimum(of: block, get: quote_price)
       close_price: maximum(of: block, get: quote_price)
       tradeAmount(in: USD)
@@ -83,12 +73,12 @@ class BitQuery:
 
 class Processing:
 
-    def read_files(self):
+    def read_files(self, file_name):
         """
         Reads all files from Data/Text/*.txt and returns a dictionary with exchange pair names as keys, and JSON dictionaries as values
         :return: Dictionary
         """
-        file_paths = glob.glob("Data/Text/*.txt")
+        file_paths = glob.glob(f"Data/Text/{file_name}.txt")
         coins_dict = {}
         for path in file_paths:
             f_name = path.rsplit('/')[2][:-4]
@@ -99,24 +89,36 @@ class Processing:
 
         return coins_dict
 
-    def txt_into_pd(self, coins_dict, name):
+    def txt_into_pd(self, coins_dict, save=False):
         """
-
+        Converts dictionary of JSON data into dictionary containing pandas DataFrames
+        Saves dataframes to CSV if requested
         :param coins_dict: Dictionary of coin paris from read_file
         :return:
         """
+        name = list(coins_dict.keys())[0]
+        coins_dict = coins_dict[name]['data']['ethereum']['dexTrades']
         coins_df_dict = {}
         temp = []
 
         for i in coins_dict:
             temp.append(self.flatten(i))
 
-        for coin in coins_dict:
-            df = pd.DataFrame(temp)
-            coins_df_dict[name] = df
+        df = pd.DataFrame(temp)
+        coins_df_dict[name] = df
+        if save:
+            df.to_csv(f"Data/CSV/{name}.csv")
+
         return coins_df_dict
 
     def flatten(self, d, parent_key='', sep='_'):
+        """
+        Converts nested dictionaries into flattened ones
+        :param d:
+        :param parent_key:
+        :param sep:
+        :return:
+        """
         items = []
         for k, v in d.items():
             new_key = parent_key + sep + k if parent_key else k
@@ -129,13 +131,18 @@ class Processing:
 
 if __name__ == '__main__':
 
+    t1 = time.time()
+    token_adresses = [["VML", "0xD302c09BC32aEF53146B6bA7BC420F5CACa897f6"], ["FCP", "0x155e8A74dAC3d8560DDaBBc26aa064B764535193"]]
+    for ta in token_adresses:
+        bq = BitQuery(ta[1], "0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c", "2021-11-01", 1)
+        result = bq.run_query()
+        bq.save_query(result, ta[0])
+        token = Processing().read_files(ta[0])
+        df = Processing().txt_into_pd(token, save=True)
+        print(df[ta[0]].tail())
 
-    bq = BitQuery()
-    tokens = Processing().read_files()
-    pand = Processing().txt_into_pd(tokens['VML_WBNB']['data']['ethereum']['dexTrades'], 'VML_WBNB')
-    print(pand['VML_WBNB'].head())
-
-
+    t2 = time.time()
+    print(t2-t1)
 
 
 
