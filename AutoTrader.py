321c135d67f2bc37d7eb159798abd0d275d7c322
Sebastian Sukiennik <sebastiansukiennik01@gmail.com>
2022-02-16 20:53:24 +0100

    Dodane zczytywanie supply dla listy tokenów

--- AutoTrader.py
+++ AutoTrader.py
@@ -1,10 +1,12 @@
 import json
 
+import pandas as pd
 import requests
 
-import undetected_chromedriver as uc
-from selenium import webdriver
-import time
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+
 
 class BitTimes:
     url = "https://thebittimes.com/list-coins-BSC.html"
@@ -13,7 +15,8 @@ class BitTimes:
     def get_token_addresses():
         """
         Scrapes token addresses from https://thebittimes.com/list-coins-BSC.html
-        :return: list
+
+        :return: list of addresses
         """
         resp = requests.get(BitTimes.url)
         resp_list = resp.text.split('<tr data-network="BSC">')
@@ -25,23 +28,76 @@ class BitTimes:
 
         return token_addresses
 
+
 class BscScan:
 
     @staticmethod
-    def get_token_holders(addresses):
+    def get_tokens_transactions(addresses):
+        """
+        Calls bscscan api and gets list of transactions for each token in provided list of adresses.
+
+        :param addresses: list of tokens adresses
+        :return: Dictionary { address : transactions_dataframe }
+        """
+        transactions = {}
         for a in addresses:
-            bsc_token_url = f'https://bscscan.com/token/{a}'
-            resp = requests.get(bsc_token_url, headers=header)
+            print(a)
+            url = f"https://api.bscscan.com/api?module=logs&action=getLogs&fromBlock=0&toBlock=latest&address={a}&apikey=Z6S9YGXET85E39HPUB4UANGEXTZ4PUQD2J"
+            resp = requests.get(url)
+            data = json.loads(resp.text)['result']
 
-    def get_tokens_transactions(self, addresses):
-        url = "https://api.bscscan.com/api?module=logs&action=getLogs&fromBlock=0&toBlock=latest&address=0x50c92a856eca4935f2c28601fa4036d1e7fd1690&apikey=Z6S9YGXET85E39HPUB4UANGEXTZ4PUQD2J"
-        resp = requests.get(url)
-        data = json.loads(resp.text)['result']
-        for d in data:
-            print(d)
+            df = pd.DataFrame()
+            for d in data:
+                if(len(d['topics']) == 3):
+                    d['topics_0'] = d['topics'][0]
+                    d['topics_1'] = d['topics'][1]
+                    d['topics_2'] = d['topics'][2]
+                elif(len(d['topics']) == 2):
+                    d['topics_0'] = d['topics'][0]
+                    d['topics_1'] = d['topics'][1]
+                elif(len(d['topics']) == 1):
+                    d['topics_0'] = d['topics'][0]
+                del d['topics']
+                df = df.append(d, ignore_index=True)
+            transactions[a] = df
 
-addresses = BitTimes.get_token_addresses()
-header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'}
+        return transactions
 
-BscScan().get_tokens_transactions(addresses)
+    @staticmethod
+    def get_tokens_holders(addresses):
+        """
+        Gets holders
+        :param addresses:
+        :return:
+        """
+        pass
+
+    @staticmethod
+    def get_tokens_supply(addresses):
+        """
+        Calls bscscan api and gets total supply for each token in provided list of adresses.
+
+        :param addresses: list of tokens adresses
+        :return: Dictionary { address : transactions_dataframe }
+        """
+        param = {'module': 'stats', 'action': 'tokensupply', 'contractaddress': '', 'apikey': 'Z6S9YGXET85E39HPUB4UANGEXTZ4PUQD2J'}
+        supplies = {}
+        for a in addresses:
+            print(a)
+            param['contractaddress'] = a
+            resp = requests.get('https://api.bscscan.com/api', params=param)
+            data = json.loads(resp.text)
+            supplies[a] = data['result']
+
+        return supplies
+
+
+addr = BitTimes.get_token_addresses()
+tran = BscScan.get_tokens_transactions(addr)
+supp = BscScan.get_tokens_supply(addr)
+
+
+print(supp)
+
+
 
