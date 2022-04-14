96b4c3ef13bf30679271aa5cfbfd973465ecab9b
Sebastian Sukiennik <sebastiansukiennik01@gmail.com>
2022-04-14 17:15:43 +0200

    test versio

--- testing.py
+++ testing.py
@@ -2,6 +2,8 @@ from TokenScraper import *
 
 tokens_tobuy = Assisting.get_new_data()
 #tokens_tobuy = pd.read_csv("TokensToBuy.csv", index_col=0)
+
+print(tokens_tobuy)
 email = "spoko369@gmail.com"
 passw = "Spoko#258"
 to = ['sebsuk2137@gmail.com', ]
@@ -10,4 +12,6 @@ if not tokens_tobuy.empty:
     tresc = tokens_tobuy['address']
     Assisting.send_email(tresc, email, passw, to_address=to)
 else:
+    #tresc = "nic nie ma ale dzialam".encode('utf-8').strip()
+    #Assisting.send_email(tresc, email, passw, to_address=to)
     print("Nie znaleziono nowych tokenow spelniajacych warunki")
